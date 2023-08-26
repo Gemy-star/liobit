fd56337b0b735ee4d95219f529047574d7d319c1
mohamed.g.ismail@orange.com <mohamed.g.ismail@orange.com>
2023-08-26 12:42:23 +0300

    first version for maal

--- main/views.py
+++ main/views.py
@@ -4,11 +4,11 @@ from .models import ContactRequests, CategoryOfBusiness, Projects
 
 def home_page(request):
     cats = CategoryOfBusiness.objects.all()
-    return render(request, 'main/home.html', context={"cats": cats})
+    return render(request, 'main/home_en.html', context={"cats": cats})
 
 
 def about_page(request):
-    return render(request, 'main/about.html')
+    return render(request, 'main/about_en.html')
 
 
 def contact_page(request):
@@ -22,10 +22,10 @@ def contact_page(request):
             return redirect('home-page')
         else:
             return redirect('contact-page')
-    return render(request, 'main/contact.html')
+    return render(request, 'main/contact_ar.html')
 
 
 def porjects(request, pk):
     projs = Projects.objects.filter(category=pk)
     context = {"projs": projs, "cat": CategoryOfBusiness.objects.get(pk=pk)}
-    return render(request, 'main/projects.html', context=context)
+    return render(request, 'main/projects_ar.html', context=context)
