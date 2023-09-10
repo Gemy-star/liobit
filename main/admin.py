50821854c576ef13f6c410c4d45e9dcf1db43c63
Mohamed Ismail <mohamed.g.ismail@orange.com>
2023-09-10 18:45:13 +0300

    final before submit

--- main/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import ContactRequests, CategoryOfBusiness, Projects
-
-admin.site.register(ContactRequests)
-admin.site.register(CategoryOfBusiness)
-admin.site.register(Projects)
