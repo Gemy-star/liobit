287240f67087227d2aca52d429625a8a7c134923
Mohamed Ismail <mohamed.g.ismail@orange.com>
2023-12-03 19:19:31 +0200

    add all

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,8 @@ from main import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home_page, name='home-page'),
-    path('home', views.home_page, name='home-index'),
-    path('contact', views.contact_page, name='contact-page'),
-    path('category/<int:pk>', views.porjects, name='cat-page'),
-    path('about', views.about_page, name='about-page'),
-
+    path("", views.home_page, name="home-page"),
+    path("home", views.home_page, name="home-index"),
+    path("maal/versions", views.porjects, name="versions-page"),
+    path("about", views.about_page, name="about-page"),
 ]
--- main/views.py
+++ main/views.py
@@ -4,28 +4,26 @@ from .models import ContactRequests, CategoryOfBusiness, Projects
 
 def home_page(request):
     cats = CategoryOfBusiness.objects.all()
-    return render(request, 'main/home.html', context={"cats": cats})
+    return render(request, "main/home.html", context={"cats": cats})
 
 
 def about_page(request):
-    return render(request, 'main/about.html')
+    return render(request, "main/about.html")
 
 
 def contact_page(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
         contact = ContactRequests(personName=name, message=message, email=email)
         contact.save()
         if contact.pk:
-            return redirect('home-page')
+            return redirect("home-page")
         else:
-            return redirect('contact-page')
-    return render(request, 'main/contact_ar.html')
+            return redirect("contact-page")
+    return render(request, "main/contact_ar.html")
 
 
-def porjects(request, pk):
-    projs = Projects.objects.filter(category=pk)
-    context = {"projs": projs, "cat": CategoryOfBusiness.objects.get(pk=pk)}
-    return render(request, 'main/projects_ar.html', context=context)
+def porjects(request):
+    return render(request, "main/projects.html")
